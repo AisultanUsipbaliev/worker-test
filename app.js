05f8f4c01c1b817a05ca074ec730db75290bdc03
aisultan <aisultanusipbaliev@gmail.com>
2021-08-23 01:40:09 +0600

    init

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
 require('dotenv').config()
 
 const express = require('express')
-const app = express()
 const http = require('http')
 
 const utilities = require('./utilities')
 const workerPoolController = require('./worker-pool/controller')
 const workerController = require('./worker/controller')
 
+const app = express()
+const server = http.createServer(app)
+
+const PORT = process.env.PORT
+const PASSWORD = 'This is a long password'
+
 app.get('/bcrypt-worker-pool', async (req, res) => {
-  const password = 'This is a long password'
   const workerPool = workerPoolController.get()
-  const result = await workerPool.bcryptHash(password)
+  const result = await workerPool.bcryptHash(PASSWORD)
 
   res.send(result)
 })
 
 app.get('/bcrypt-worker', async (req, res) => {
-  const password = 'This is a long password'
-  const result = await workerController.bcryptHash(password)
+  const result = await workerController.bcryptHash(PASSWORD)
 
   res.send(result)
 })
 
 app.get('/bcrypt', async (req, res) => {
-  const password = 'This is a long password'
-  const result = await utilities.bcryptHash(password)
+  const result = await utilities.bcryptHash(PASSWORD)
 
   res.send(result)
 })
-
-const port = process.env.PORT
-const server = http.createServer(app)
 
 ;(async () => {
   if (process.env.WORKER_POOL_ENABLED === '1') {
@@ -39,7 +38,7 @@ const server = http.createServer(app)
     await workerPoolController.init(options)
   }
 
-  server.listen(port, () => {
-    console.log('NodeJS Performance Optimizations listening on: ', port)
+  server.listen(PORT, () => {
+    console.log('NodeJS Performance Optimizations listening on: ', PORT)
   })
 })()
